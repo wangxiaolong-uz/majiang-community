1afcb137386e596003c8e70448a0eaedb1c7c97f
25467 <254670122@qq.com>
2020-03-03 15:55:41 +0800

    add index and bootstrap

--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
-资料
+#资料
 [Spring 文档](https://spring.io/guides)
 [Spring Web](https://spring.io/guides/gs/serving-web-content/) 
 
-工具
-git 
+#工具
+git.exe
+
+#怎么显示啊
--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -5,21 +5,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.security.PublicKey;
-
 /**
  * @author wxl
  * @date 2020/3/3 - 11:56
  */
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model)
+    @GetMapping("/")
+    public String index()
     {
-        model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 
 }
